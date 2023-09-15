2a8e7975cc424115f0b14b3f8939aa950946ca89
MADHUBALASUBRAMANIYAM <142659646+MADHUBALASUBRAMANIYAM@users.noreply.github.com>
2023-09-15 20:33:12 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,52 @@
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+data = pd.read_csv('data.csv')
+
+# Data Preprocessing
+# Handle missing values
+data['Gender'].fillna(data['Gender'].mode()[0], inplace=True)
+data['Married'].fillna(data['Married'].mode()[0], inplace=True)
+data['Dependents'].fillna(data['Dependents'].mode()[0], inplace=True)
+data['Self_Employed'].fillna(data['Self_Employed'].mode()[0], inplace=True)
+data['LoanAmount'].fillna(data['LoanAmount'].median(), inplace=True)
+data['Loan_Amount_Term'].fillna(data['Loan_Amount_Term'].mode()[0], inplace=True)
+data['Credit_History'].fillna(data['Credit_History'].mode()[0], inplace=True)
+
+# Encode categorical variables
+label_encoder = LabelEncoder()
+data['Gender'] = label_encoder.fit_transform(data['Gender'])
+data['Married'] = label_encoder.fit_transform(data['Married'])
+data['Dependents'] = label_encoder.fit_transform(data['Dependents'])
+data['Education'] = label_encoder.fit_transform(data['Education'])
+data['Self_Employed'] = label_encoder.fit_transform(data['Self_Employed'])
+data['Property_Area'] = label_encoder.fit_transform(data['Property_Area'])
+data['Loan_Status'] = label_encoder.fit_transform(data['Loan_Status'])
+
+# Feature selection
+X = data.drop(columns=['Loan_ID', 'Loan_Status'])
+y = data['Loan_Status']
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a Logistic Regression model
+model = LogisticRegression(random_state=42)
+model.fit(X_train, y_train)
+
+# Predict loan status on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print("Confusion Matrix:\n", conf_matrix)
+print("Classification Report:\n", class_report)
